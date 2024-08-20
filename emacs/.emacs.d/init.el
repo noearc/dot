35eeeadc2430a32870e62be3ca51d060bc6d93ae
n451 <2020200706@ruc.edu.cn>
2024-08-20 17:36:25 +0800

    add emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+ '(package-selected-packages
+   '(eshell-syntax-highlighting eshell-toggle lsp-ivy lsp-ui lsp-mode pyim rime company-box company-lua haskell-mode lua-mode evil-easymotion avy smart-mode-line undo-tree amx general evil-collection magit counsel all-the-icons-ivy-rich toc-org org-bullets nerd-icons-completion which-key evil-terminal-cursor-changer nerd-icons ner-icons doom-themes doom-modeline evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
